f54c5870b87bdea540ed3541eab2f6b176cc793c
derpen <rizqyassabil@hotmail.com>
2025-08-23 19:42:10 +0700

    Added model. Need some flashlight so I can see anything

--- theStaircases/main.js
+++ theStaircases/main.js
@@ -1,1 +1,25 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("rgb(255, 0, 0)");
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.001, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+camera.position.z = 18;
+camera.position.y = 3;
+
+const loader = new GLTFLoader();
+loader.load( 'models/staircase.glb', function ( gltf ) {
+  scene.add( gltf.scene );
+}, undefined, function ( error ) {
+  console.error( error );
+} );
+
+function animate() {
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
